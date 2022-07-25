f7682d42411cc6b13da864573de86f8e51dfb053
kosarc <petiakraj@gmail.com>
2022-07-25 16:33:36 +0300

    improved app

--- JS/weatherApp.js
+++ JS/weatherApp.js
@@ -1,35 +1,28 @@
-//The main function
+//Forecast day feature
 
 function nextDaysForecats(response) {
-  console.log(response);
   let forecastDayNames = response.data.daily;
-  let dailyForecastSelector = document.querySelector("#daily-forecast");
   let snowDailyForecast = `<div class="row">`;
-  let weatherIconForecast = {
-    Clouds: `<i class="fa-solid fa-cloud"></i>`,
-    Rain: `<i class="fa-solid fa-cloud-rain"></i>`,
-    Clear: `<i class="fa-solid fa-sun"></i>`,
-  };
+  let idToForecast = 1;
+  let classToForecst = 1;
 
   forecastDayNames.forEach(function (forecastDays, index) {
     let weatherDescriptionForecast = forecastDays.weather[0].main;
-
     let data = new Date(forecastDays.dt * 1000);
     let day = data.getDay();
     let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
     if (index < 4) {
-      snowDailyForecast += `<div class="col">
+      snowDailyForecast += ` <div  class="col">
                   <div class="card dashboard" style="width: 8rem">
-                    <div class="card-body">
-                      <p class="card-text">${days[day]} <br /> ${
+                    <div id="${++idToForecast}" class="card-body ${++classToForecst}">
+                      <class="card-text">${days[day]} <br /> ${
         weatherIconForecast[weatherDescriptionForecast]
       }
-                      <br /> <span class="temp-max"> ${Math.round(
+                      <br /> <span class="max-temp"> ${Math.round(
                         forecastDays.temp.max
-                      )}</span>°<span class="temp-min"> ${Math.round(
+                      )}º</span> <span class="min-temp"> ${Math.round(
         forecastDays.temp.min
-      )}°</span> 
+      )}º</span>
                     </div>
                   </div>
                 </div> `;
@@ -37,95 +30,57 @@ function nextDaysForecats(response) {
       dailyForecastSelector.innerHTML = snowDailyForecast;
     }
   });
+  tempInfo = forecastDayNames;
   snowDailyForecast += `</div>`;
+  console.log(tempInfo);
 }
-let example = new Date(1659002400 * 1000);
-console.log(example);
-
 function getLocationCoords(response) {
-  let apiKey = "21cf52b64168334a0b71f4d075758440";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${response.data.coord.lat}&lon=${response.data.coord.lon}&appid=${apiKey}&units=metric`;
   axios(apiUrl).then(nextDaysForecats);
 }
 
-function getLocationCoordsCurrent(posission) {
-  let apiKey = "21cf52b64168334a0b71f4d075758440";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${posission.coords.latitude}&lon=${posission.coords.longitude}&appid=${apiKey}&units=metric`;
-  axios(apiUrl).then(nextDaysForecats);
-}
+// Main function
 
 function initialData(cityInfo) {
   function initialPosition(response) {
     getLocationCoords(response);
-    let weatherIcon = {
-      Clouds: `<i class="fa-solid fa-cloud"></i>`,
-      Rain: `<i class="fa-solid fa-cloud-rain"></i>`,
-      Clear: `<i class="fa-solid fa-sun"></i>`,
-    };
-    let snowWeatherIcon = document.querySelector("#weather-icon");
+
+    tempCurrent = response.data.main.temp;
+
     let weatherDescription = response.data.weather[0].main;
-    snowWeatherIcon.innerHTML = weatherIcon[weatherDescription];
-    let snowWeather = document.querySelector("#weather-description");
-    snowWeather.innerHTML = weatherDescription;
     let pressure = response.data.main.pressure;
-    let snowPessure = document.querySelector("#pressure");
-    snowPessure.innerHTML = pressure;
-    let wind = response.data.wind.speed;
-    let snowWind = document.querySelector("#wind");
-    let windRound = Math.round(wind);
-    snowWind.innerHTML = windRound;
+    windSpeedUnit = response.data.wind.speed;
+    let windRound = Math.round(windSpeedUnit);
     let humidity = response.data.main.humidity;
-    let snowHumidity = document.querySelector("#humidity");
-    snowHumidity.innerHTML = humidity;
-    let tempCurrent = response.data.main.temp;
     let tempRound = Math.round(tempCurrent);
-    let snowTemp = document.querySelector("#temperature");
-    snowTemp.innerHTML = `${tempRound}º`;
     let city = response.data.name;
-    let snowCity = document.querySelector("#city");
+
+    snowWeatherIcon.innerHTML = weatherIconForecast[weatherDescription];
+    snowWeather.innerHTML = weatherDescription;
+    snowPessure.innerHTML = pressure;
+    snowWind.innerHTML = `${windRound} km/h`;
+    snowHumidity.innerHTML = humidity;
+    snowTemp.innerHTML = `${tempRound}º`;
     snowCity.innerHTML = city;
-
-    function convertToCelsius() {
-      let temp = document.querySelector("#temperature");
-      temp.innerHTML = `${Math.round(tempCurrent)}º`;
-      document.querySelector("#celsius").classList.remove("change");
-      document.querySelector("#fahrenheit").classList.add("change");
-    }
-
-    function convertToFahrenheit() {
-      let temp = document.querySelector("#temperature");
-      temp.innerHTML = `${Math.round(tempCurrent * 1.8 + 32)}º`;
-      document.querySelector("#celsius").classList.add("change");
-      document.querySelector("#fahrenheit").classList.remove("change");
-    }
-
-    let tempCelsius = document.querySelector("#celsius");
-    let tempFahrinheit = document.querySelector("#fahrenheit");
-    tempCelsius.addEventListener("click", convertToCelsius);
-    tempFahrinheit.addEventListener("click", convertToFahrenheit);
   }
-  function city(event) {
+  function cityValue(event) {
     event.preventDefault();
     let input = document.querySelector("#exampleInputEmail1").value;
     initialData(input);
   }
   let cityName = cityInfo;
-  let apiKey = "21cf52b64168334a0b71f4d075758440";
   let unit = "metric";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=${unit}`;
   axios.get(apiUrl).then(initialPosition);
-
-  let searchForm = document.querySelector("#search-line");
-  searchForm.addEventListener("submit", city);
+  searchForm.addEventListener("submit", cityValue);
 }
 
-//The current Button
+//Current Button
 
 function currentLocation() {
   function snowLocationName(response) {
     let city = response.data[0].name;
-    let snowCity = document.querySelector("#city");
-    snowCity.innerHTML = city;
+    initialData(city);
   }
   function snowLocation(posission) {
     let lat = posission.coords.latitude;
@@ -133,63 +88,7 @@ function currentLocation() {
     let apiUrlReverse = `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${apiKey}`;
     axios.get(apiUrlReverse).then(snowLocationName);
   }
-  function currentPositionWeather(response) {
-    let weatherIcon = {
-      Clouds: `<i class="fa-solid fa-cloud"></i>`,
-      Rain: `<i class="fa-solid fa-cloud-rain"></i>`,
-      Clear: `<i class="fa-solid fa-sun"></i>`,
-    };
-    let snowWeatherIcon = document.querySelector("#weather-icon");
-    let weatherDescription = response.data.weather[0].main;
-    snowWeatherIcon.innerHTML = weatherIcon[weatherDescription];
-    let snowWeather = document.querySelector("#weather-description");
-    snowWeather.innerHTML = weatherDescription;
-    let pressure = response.data.main.pressure;
-    let snowPessure = document.querySelector("#pressure");
-    snowPessure.innerHTML = pressure;
-    let wind = response.data.wind.speed;
-    let snowWind = document.querySelector("#wind");
-    let windRound = Math.round(wind);
-    snowWind.innerHTML = windRound;
-    let humidity = response.data.main.humidity;
-    let snowHumidity = document.querySelector("#humidity");
-    snowHumidity.innerHTML = humidity;
-    let tempCurrent = response.data.main.temp;
-    let tempRound = Math.round(tempCurrent);
-    let snowTemp = document.querySelector("#temperature");
-    snowTemp.innerHTML = `${tempRound}º`;
-
-    function convertToCelsius() {
-      let temp = document.querySelector("#temperature");
-
-      temp.innerHTML = `${Math.round(tempCurrent)}º`;
-    }
-
-    function convertToFahrenheit() {
-      let temp = document.querySelector("#temperature");
-
-      temp.innerHTML = `${Math.round(tempCurrent * 1.8 + 32)}º`;
-    }
-
-    let tempCelsius = document.querySelector("#celsius");
-    let tempFahrinheit = document.querySelector("#fahrenheit");
-    tempCelsius.addEventListener("click", convertToCelsius);
-    tempFahrinheit.addEventListener("click", convertToFahrenheit);
-  }
-
-  function currentLocationWeather(posission) {
-    let lat = posission.coords.latitude;
-    let lon = posission.coords.longitude;
-    let unit = "metric";
-    let endpoint = "https://api.openweathermap.org/data/2.5/weather?";
-    let apiUrl = `${endpoint}lat=${lat}&lon=${lon}&appid=${apiKey}&units=${unit}`;
-    axios.get(apiUrl).then(currentPositionWeather);
-  }
   navigator.geolocation.getCurrentPosition(snowLocation);
-  navigator.geolocation.getCurrentPosition(currentLocationWeather);
-  navigator.geolocation.getCurrentPosition(getLocationCoordsCurrent);
-
-  let apiKey = "21cf52b64168334a0b71f4d075758440";
 }
 
 function properMinutes() {
@@ -199,7 +98,31 @@ function properMinutes() {
   return minutes;
 }
 
-//Variables
+//Convert F/C function button
+
+function convertToCelsius() {
+  for (let i = 0; i < 4; i++) {
+    maxTemp[i].innerHTML = `${Math.round(tempInfo[i].temp.max)}º`;
+    minTemp[i].innerHTML = ` ${Math.round(tempInfo[i].temp.min)}º`;
+  }
+  tempSign.innerHTML = `${Math.round(tempCurrent)}º`;
+  tempCelsius.classList.remove("change");
+  tempFahrinheit.classList.add("change");
+  snowWind.innerHTML = `${Math.round(windSpeedUnit)} km/h`;
+}
+
+function convertToFahrenheit() {
+  for (let i = 0; i < 4; i++) {
+    maxTemp[i].innerHTML = `${Math.round(tempInfo[i].temp.max * 1.8 + 32)}º`;
+    minTemp[i].innerHTML = ` ${Math.round(tempInfo[i].temp.min * 1.8 + 32)}º`;
+  }
+  tempSign.innerHTML = `${Math.round(tempCurrent * 1.8 + 32)}º`;
+  tempCelsius.classList.add("change");
+  tempFahrinheit.classList.remove("change");
+  snowWind.innerHTML = `${Math.round(windSpeedUnit * 0.62137)} mi/h`;
+}
+
+//Current date
 
 let time = new Date();
 let days = time.getDay();
@@ -238,31 +161,110 @@ if (hours < 10) {
 }
 let currentTime = `${hours}:${properMinutes()}`;
 let currentDate = `${date} ${currentMonth} ${year}`;
+
+//Icon object
+
+let weatherIconForecast = {
+  Clouds: `<i class="fa-solid fa-cloud"></i>`,
+  Rain: `<i class="fa-solid fa-cloud-rain"></i>`,
+  Clear: `<i class="fa-solid fa-sun"></i>`,
+};
+
+//Global variables
+
+let apiKey = "21cf52b64168334a0b71f4d075758440";
+let tempInfo = [];
+let tempCurrent = null;
+let windSpeedUnit = null;
+
 let weatherDay = document.querySelector("#current-day");
-weatherDay.innerHTML = currentDay;
-let weatherTime = document.querySelector("#local-time");
-weatherTime.innerHTML = currentTime;
+let tempSign = document.querySelector("#temperature");
+let tempCelsius = document.querySelector("#celsius");
+let tempFahrinheit = document.querySelector("#fahrenheit");
+let maxTemp = document.getElementsByClassName("max-temp");
+let minTemp = document.getElementsByClassName("min-temp");
+let currentButton = document.querySelector("#current-button");
 let weatherDate = document.querySelector("#local-date");
+let weatherTime = document.querySelector("#local-time");
+let snowWind = document.querySelector("#wind");
+let snowWeatherIcon = document.querySelector("#weather-icon");
+let snowCity = document.querySelector("#city");
+let snowTemp = document.querySelector("#temperature");
+let snowWeather = document.querySelector("#weather-description");
+let snowPessure = document.querySelector("#pressure");
+let snowHumidity = document.querySelector("#humidity");
+let dailyForecastSelector = document.querySelector("#daily-forecast");
+let searchForm = document.querySelector("#search-line");
+let forecastButton = document.getElementsByClassName("col");
+
+document.addEventListener("click", function (e) {
+  if (e.target && e.target.id == "2") {
+    snowHumidity.innerHTML = tempInfo[0].humidity;
+    snowPessure.innerHTML = tempInfo[0].pressure;
+    snowWind.innerHTML = Math.round(tempInfo[0].wind_speed);
+
+    console.log(tempInfo);
+    snowTemp.innerHTML = maxTemp[0].innerHTML;
+    document.getElementById("2").classList.add("bg");
+    document.getElementById("3").classList.remove("bg");
+    document.getElementById("4").classList.remove("bg");
+    document.getElementById("5").classList.remove("bg");
+  }
+});
+
+document.addEventListener("click", function (e) {
+  if (e.target && e.target.id == "3") {
+    snowHumidity.innerHTML = tempInfo[1].humidity;
+    snowPessure.innerHTML = tempInfo[1].pressure;
+    snowWind.innerHTML = Math.round(tempInfo[1].wind_speed);
+    snowTemp.innerHTML = maxTemp[1].innerHTML;
+    document.getElementById("2").classList.remove("2");
+    document.getElementById("3").classList.add("bg");
+    document.getElementById("2").classList.remove("bg");
+    document.getElementById("4").classList.remove("bg");
+    document.getElementById("5").classList.remove("bg");
+  }
+});
+
+document.addEventListener("click", function (e) {
+  if (e.target && e.target.id == "4") {
+    snowHumidity.innerHTML = tempInfo[2].humidity;
+    snowPessure.innerHTML = tempInfo[2].pressure;
+    snowWind.innerHTML = Math.round(tempInfo[2].wind_speed);
+    snowTemp.innerHTML = maxTemp[2].innerHTML;
+    document.getElementById("2").classList.remove("2");
+    document.getElementById("4").classList.add("bg");
+    document.getElementById("3").classList.remove("bg");
+    document.getElementById("2").classList.remove("bg");
+    document.getElementById("5").classList.remove("bg");
+  }
+});
+
+document.addEventListener("click", function (e) {
+  if (e.target && e.target.id == "5") {
+    snowHumidity.innerHTML = tempInfo[3].humidity;
+    snowPessure.innerHTML = tempInfo[3].pressure;
+    snowWind.innerHTML = Math.round(tempInfo[3].wind_speed);
+    snowTemp.innerHTML = maxTemp[3].innerHTML;
+
+    document.getElementById("2").classList.remove("2");
+    document.getElementById("5").classList.add("bg");
+    document.getElementById("3").classList.remove("bg");
+    document.getElementById("2").classList.remove("bg");
+    document.getElementById("4").classList.remove("bg");
+  }
+});
+
+weatherDay.innerHTML = currentDay;
+weatherTime.innerHTML = currentTime;
 weatherDate.innerHTML = currentDate;
 
-let currentButton = document.querySelector("#current-button");
+//Buttons
+
 currentButton.addEventListener("click", currentLocation);
+tempCelsius.addEventListener("click", convertToCelsius);
+tempFahrinheit.addEventListener("click", convertToFahrenheit);
+
+//Launch of the main function
+
 initialData("Kyiv");
-
-let forecastDayNames = ["Sun", "Mon", "Tue", "Wed"];
-let dailyForecastSelector = document.querySelector("#daily-forecast");
-let snowDailyForecast = `<div class="row">`;
-
-forecastDayNames.forEach(function (forecastDays) {
-  snowDailyForecast += `<div class="col">
-                  <div class="card dashboard" style="width: 8rem">
-                    <i class="fa-solid fa-sun icon"></i>
-                    <div class="card-body">
-                      <p class="card-text">${forecastDays} <br />29°C</p>
-                    </div>
-                  </div>
-                </div>`;
-
-  dailyForecastSelector.innerHTML = snowDailyForecast;
-});
-snowDailyForecast += `</div>`;
